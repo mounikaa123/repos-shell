eeb14a120243396ce49cd6d587b7672b666db681
A <Vijaya1999bsc@gmail.com>
2023-10-18 18:29:17 +0530

    project

--- 01.frontend.sh
+++ 01.frontend.sh
@@ -1,10 +1,15 @@
+echo -e "\e[33m INSTALLING NGINX SERVICE \e[0m"
 yum install nginx -y
+echo -e "\e[32m REMOVING DEFAULT NGINX CONTENT \e[0m"
 cd /usr/share/nginx/html
 rm -rf *
+echo -e  "\e[31m DOWNLOADING NEW CONTENT TO NGINX \e[0m"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 unzip frontend.zip
 rm -rf frontend.zip
+echo -e  "\e[30mCONFIGURING REVERSE PROXY SERVER \e[0m"
 cp /root/repos-shell/roboshop.conf  /etc/nginx/default.d/roboshop.conf
+echo -e "\e[34m ENABLING AND RESTARTING NGINX \e[0m"
 systemctl enable nginx
 systemctl restart nginx
 
