63dc844db7d5d1ea4a1288165fbd1a75cde70a2e
A <Vijaya1999bsc@gmail.com>
2023-10-19 20:47:35 +0530

    project

--- 05.user.sh
+++ 05.user.sh
@@ -11,13 +11,13 @@ curl -O https://roboshop-artifacts.s3.amazonaws.com/user.zip
 unzip user.zip
 rm -rf user.zip
 npm install
-echo -e "\e[32m CREATING USER SERVICE \e[0m"
+echo -e "\e[32m CREATING user SERVICE \e[0m"
 cp /root/repos-shell/user.service /etc/systemd/system/user.service
 echo -e "\e[32m DOWNLOADING AND INSTALLING THE MONGODB SCHEMA \e[0m"
 cp /root/repos-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
 yum install mongodb-org-shell -y
 mongo --host mongodb-dev.mounika.site </app/schema/user.js
-echo -e "\e[32m ENABLING AND STARTING THE USER SERVICE \e[0m"
+echo -e "\e[32m ENABLING AND STARTING THE user SERVICE \e[0m"
 systemctl daemon-reload
 systemctl enable user
 systemctl restart user
