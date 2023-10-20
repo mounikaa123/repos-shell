49581b2de26e0ef1966ac6c89e80bf212fcf99e8
A <Vijaya1999bsc@gmail.com>
2023-10-20 18:18:29 +0530

    project

--- 09.rabbitmq.sh
+++ 09.rabbitmq.sh
@@ -6,6 +6,7 @@ echo -e "\e[33m INSTALLING RABBITMQ SERVICE \e[0m"
 yum install rabbitmq-server -y
 echo -e "\e[33m CREATION OF USER FOR THE APPLICATION \e[0m"
 rabbitmqctladd_userroboshop roboshop123
+rabbitmqctlset_permissions -p / roboshop".*"".*"".*"
 echo -e "\e[32m ENABLING AND STARTING THE rabbitmq SERVICE \e[0m"
 systemctl daemon-reload
 systemctl enable rabbitmq
