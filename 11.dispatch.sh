81e12f0d9a795daa727ac6333cd6f6b99fb3ea1e
A <Vijaya1999bsc@gmail.com>
2023-10-20 18:53:05 +0530

    project

--- 11.dispatch.sh
+++ 11.dispatch.sh
@@ -6,9 +6,12 @@ mkdir /app
 cd /app
 echo -e "\e[32m DOWNLOADING NEW CONTENT AND DEPENDENCIES \e[0m"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/dispatch.zip
-unzip /tmp/dispatch.zip
+unzip dispatch.zip
 rm -rf dispatch.zip
 go mod init dispatch
 go get
 go build
-
+echo -e "\e[32m ENABLING AND STARTING THE DISPATCH SERVICE \e[0m"
+systemctl daemon-reload
+systemctl enable dispatch
+systemctl restart dispatch
