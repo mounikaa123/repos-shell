ed05bd6c062c30b51c04e28d1af1657ab3036ef4
A <Vijaya1999bsc@gmail.com>
2023-10-20 13:53:04 +0530

    project

--- 07.mysql.sh
+++ 07.mysql.sh
@@ -1,7 +1,7 @@
 echo -e "\e[32m DISABLING MySql SERVICE \e[0m"
 yum module disable mysql -y
 echo -e  "\e[32m SETTING MySql REPO \e[0m"
-cp /root/repos-shell/MySql.repo /etc/yum.repos.d/mysql.repo
+cp /root/repos-shell/mysql.repo /etc/yum.repos.d/mysql.repo
 echo -e "\e[32m INSTALLING MySql SERVER \e[0m"
 yum install mysql-community-server -y
 echo -e "\e[32m CHANGING THE DEFAULT ROOT PASSWORD \e[0m"
@@ -10,4 +10,3 @@ echo -e "\e[32m ENABLING AND STARTING THE MYSQL SERVICE \e[0m"
 systemctl daemon-reload
 systemctl enable mysql
 systemctl restart mysql
-
