e42b528d7760bcf7b260c7de246db5fab6442178
A <Vijaya1999bsc@gmail.com>
2023-10-20 16:04:15 +0530

    project

--- 08.shipping.sh
+++ 08.shipping.sh
@@ -2,6 +2,7 @@ echo -e "\e[32m INSTALLING MAVEN SERVICE \e[0m"
 yum install maven -y
 echo -e "\e[32m ADDING USER AND LOCATION \e[0m"
 useradd roboshop
+rm -rf /app
 mkdir /app
 cd /app
 echo -e "\e[32m DOWNLOADING NEW CONTENT AND DEPENDENCIES \e[0m"
