88b8f6d95d92508e350f259e6fe33326caf32f7a
A <Vijaya1999bsc@gmail.com>
2023-10-21 15:52:21 +0530

    project

--- 10.payment.sh
+++ 10.payment.sh
@@ -1,17 +1,16 @@
-echo -e "\e[33m INSTALLING PYTHON 3.6 \e[0m"
+echo -e "\e[32m Installing python server \e[0m"
 yum install python36 gcc python3-devel -y
-echo -e "\e[32m ADDING USER AND LOCATION \e[0m"
+echo -e "\e[32m Adding user and location \e[0m"
 useradd roboshop
-rm -rf /app
 mkdir /app
 cd /app
-echo -e "\e[32m DOWNLOADING NEW CONTENT AND DEPENDENCIES \e[0m"
+echo -e "\e[32m Downloading new app content and dependencies to payment server \e[0m"
 curl -O https://roboshop-artifacts.s3.amazonaws.com/payment.zip
 unzip payment.zip
 pip3.6 install -r requirements.txt
-echo -e "\e[32m CREATING PAYMENT SERVICE \e[0m"
+echo -e "\e[32m creating payment service file \e[0m"
 cp /root/repos-shell/payment.service /etc/systemd/system/payment.service
-echo -e "\e[32m ENABLING AND STARTING THE payment SERVICE \e[0m"
+echo -e "\e[32m Enabling and starting the payment service \e[0m"
 systemctl daemon-reload
 systemctl enable payment
 systemctl restart payment
