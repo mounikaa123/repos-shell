e4546cad95299665702b30d3cfcd981c6d4fac1a
A <Vijaya1999bsc@gmail.com>
2023-10-21 19:00:11 +0530

    project

--- 02.mongodb.sh
+++ 02.mongodb.sh
@@ -1,9 +1,13 @@
-echo -e  "\e[32m DOWNLOADING MONGODB REPO \e[0m"
+color="\e[32m"
+nocolor="\e[0m"
+logfile="/tmp/roboshop.log"
+
+echo -e  "$color DOWNLOADING MONGODB REPO $nocolor"
 cp /root/repos-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
-echo -e "\e[33m INSTALLING MONGODB SERVICE \e[0m"
-yum install mongodb-org -y &>>/tmp/roboshop.log
-echo -e  "\e[32m CHANGING THE LISTEN ADDRESS \e[0m"
+echo -e "\e[33m INSTALLING MONGODB SERVICE $nocolor"
+yum install mongodb-org -y &>>${logfile}
+echo -e  "$color CHANGING THE LISTEN ADDRESS $nocolor"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
-echo -e "\e[32m ENABLING AND RESTARTING MONGODB SERVICE \e[0m"
-systemctl enable mongod &>>/tmp/roboshop.log
+echo -e "$color ENABLING AND RESTARTING MONGODB SERVICE $nocolor"
+systemctl enable mongod &>>${logfile}
 systemctl restart mongod
