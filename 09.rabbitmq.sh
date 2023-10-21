a21522e62189d7fdf0a3fd64b6cb19bad9e5e302
A <Vijaya1999bsc@gmail.com>
2023-10-21 15:56:59 +0530

    project

--- 09.rabbitmq.sh
+++ 09.rabbitmq.sh
@@ -1,15 +1,14 @@
-echo -e "\e[32m CONFIGURING YUM REPOSITORIES FROM SCRIPT \e[0m"
+echo -e "\e[32m Downloading rabbitmq repo file\e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash
-echo -e "\e[32m CONFIGURING YUM REPOSITORIES FROM RABBITMQ \e[0m"
 curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash
-echo -e "\e[33m INSTALLING RABBITMQ SERVICE \e[0m"
+echo -e "\e[32m Installing Rabbitmq-server\e[0m"
 yum install rabbitmq-server -y
-echo -e "\e[32m ENABLING AND STARTING THE rabbitmq SERVICE \e[0m"
+echo -e "\e[32m Enabling and starting the Rabbitmq-server\e[0m"
 systemctl enable rabbitmq-server
-systemctl restart rabbitmq-server
-echo -e "\e[33m CREATION OF USER FOR THE APPLICATION \e[0m"
+systemctl start rabbitmq-server
+echo -e "\e[32m Adding user and setting permissions \e[0m"
 rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop".*"".*"".*"
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
 
 
 
