20b75004988b0311677561d91c90e9c7a8146e6c
A <Vijaya1999bsc@gmail.com>
2023-10-21 16:28:43 +0530

    project

--- 01.frontend.sh
+++ 01.frontend.sh
@@ -1,16 +1,20 @@
-echo -e "\e[33m INSTALLING NGINX SERVICE \e[0m"
-yum install nginx -y &>>/tmp/roboshop.log
-echo -e "\e[32m REMOVING DEFAULT NGINX CONTENT \e[0m"
+color="\e[32m"
+nocolor="\e[0m"
+logfile="/tmp/roboshop.log"
+
+echo -e "$color INSTALLING NGINX SERVICE $nocolor"
+yum install nginx -y &>>${logfile}
+echo -e "$color REMOVING DEFAULT NGINX CONTENT $nocolor"
 cd /usr/share/nginx/html
-rm -rf * &>>/tmp/roboshop.log
-echo -e  "\e[31m DOWNLOADING NEW CONTENT TO NGINX \e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
-unzip frontend.zip &>>/tmp/roboshop.log
+rm -rf * &>>${logfile}
+echo -e  "$color DOWNLOADING NEW CONTENT TO NGINX $nocolor"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${logfile}
+unzip frontend.zip &>>${logfile}
 rm -rf frontend.zip
-echo -e  "\e[30mCONFIGURING REVERSE PROXY SERVER \e[0m"
+echo -e  "$color CONFIGURING REVERSE PROXY SERVER $nocolor"
 cp /root/repos-shell/roboshop.conf  /etc/nginx/default.d/roboshop.conf
-echo -e "\e[34m ENABLING AND RESTARTING NGINX \e[0m"
-systemctl enable nginx &>>/tmp/roboshop.log
+echo -e "$color ENABLING AND RESTARTING NGINX $nocolor"
+systemctl enable nginx &>>${logfile}
 systemctl restart nginx
 
 
--- 02.mongodb.sh
+++ 02.mongodb.sh
@@ -1,9 +1,9 @@
 echo -e  "\e[32m DOWNLOADING MONGODB REPO \e[0m"
 cp /root/repos-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
 echo -e "\e[33m INSTALLING MONGODB SERVICE \e[0m"
-yum install mongodb-org -y
+yum install mongodb-org -y &>>/tmp/roboshop.log
 echo -e  "\e[32m CHANGING THE LISTEN ADDRESS \e[0m"
 sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo -e "\e[32m ENABLING AND RESTARTING MONGODB SERVICE \e[0m"
-systemctl enable mongod
+systemctl enable mongod &>>/tmp/roboshop.log
 systemctl restart mongod
