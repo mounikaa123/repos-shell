bf6bf736d744d7ecd0b9fa07140e15becbd0dcb8
A <Vijaya1999bsc@gmail.com>
2023-10-22 12:28:09 +0530

    project

--- 08.shipping.sh
+++ 08.shipping.sh
@@ -1,24 +1,30 @@
-echo -e "\e[32m INSTALLING MAVEN SERVICE \e[0m"
-yum install maven -y
-echo -e "\e[32m ADDING USER AND LOCATION \e[0m"
-useradd roboshop
-rm -rf /app
-mkdir /app
-cd /app
-echo -e "\e[32m DOWNLOADING NEW CONTENT AND DEPENDENCIES \e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/shipping.zip
-unzip shipping.zip
-mvn clean package
+color="\e[32m"
+nocolor="\e[0m"
+logfile="/tmp/roboshop.log"
+app_path="/app"
+
+
+echo -e "$color INSTALLING MAVEN SERVICE $nocolor"
+yum install maven -y &>>${logfile}
+echo -e "$color ADDING USER AND LOCATION $nocolor"
+useradd roboshop &>>${logfile}
+rm -rf ${app_path}
+mkdir ${app_path}
+cd ${app_path}
+echo -e "$color DOWNLOADING NEW CONTENT AND DEPENDENCIES $nocolor"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>${logfile}
+unzip shipping.zip &>>${logfile}
+mvn clean package &>>${logfile}
 mv target/shipping-1.0.jar shipping.jar
-echo -e "\e[32m CREATING SHIPPING SERVICE \e[0m"
+echo -e "$color CREATING SHIPPING SERVICE $nocolor"
 cp /root/repos-shell/shipping.service /etc/systemd/system/shipping.service
 systemctl daemon-reload
-echo -e "\e[32m INSTALLING MySql SERVICE \e[0m"
-yum install mysql -y
-echo -e "\e[32M LOADING MySql SCHEME \e[0m"
-mysql -h mysql-dev.mounika.site -uroot -pRoboShop@1 </app/schema/shipping.sql
-echo -e "\e[32m ENABLING AND STARTING THE SHIPPING SERVICE \e[0m"
-systemctl enable shipping
+echo -e "$color INSTALLING MySql SERVICE $nocolor"
+yum install mysql -y &>>${logfile}
+echo -e "$color LOADING MySql SCHEME $nocolor"
+mysql -h mysql-dev.mounika.site -uroot -pRoboShop@1 <${app_path}/schema/shipping.sql &>>${logfile}
+echo -e "$color ENABLING AND STARTING THE SHIPPING SERVICE $nocolor"
+systemctl enable shipping &>>${logfile}
 systemctl restart shipping
 
 
