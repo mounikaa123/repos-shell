7370afbdd050af5d9eb983013e248331309071a0
A <Vijaya1999bsc@gmail.com>
2023-10-22 12:06:13 +0530

    project

--- 05.user.sh
+++ 05.user.sh
@@ -1,26 +1,31 @@
-echo -e "\e[32m DOWNLOADING NODEJS REPO \e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-echo -e "\e[32m INSTALLING NODEJS SERVICE \e[0m"
-yum install nodejs -y
-echo -e "\e[32m ADDING USER AND LOCATION \e[0m"
-useradd roboshop
-rm -rf /app
-mkdir /app
-cd /app
-echo -e "\e[32m DOWNLOADING NEW CONTENT AND DEPENDENCIES \e[0m"
-curl -O https://roboshop-artifacts.s3.amazonaws.com/user.zip
-unzip user.zip
+color="\e[32m"
+nocolor="\e[0m"
+logfile="/tmp/roboshop.log"
+app_path="/app"
+
+echo -e "${color} DOWNLOADING NODEJS REPO ${nocolor}"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${logfile}
+echo -e "${color} INSTALLING NODEJS SERVICE ${nocolor}"
+yum install nodejs -y &>>${logfile}
+echo -e "${color} ADDING USER AND LOCATION ${nocolor}"
+useradd roboshop &>>${logfile}
+rm -rf ${app_path} 
+mkdir ${app_path}
+cd ${app_path}
+echo -e "${color} DOWNLOADING NEW CONTENT AND DEPENDENCIES ${nocolor}"
+curl -O https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>${logfile}
+unzip user.zip &>>${logfile}
 rm -rf user.zip
-npm install
-echo -e "\e[32m CREATING user SERVICE \e[0m"
+npm install &>>${logfile}
+echo -e "${color} CREATING user SERVICE ${nocolor}"
 cp /root/repos-shell/user.service /etc/systemd/system/user.service
 systemctl daemon-reload
-echo -e "\e[32m DOWNLOADING AND INSTALLING THE MONGODB SCHEMA \e[0m"
+echo -e "${color} DOWNLOADING AND INSTALLING THE MONGODB SCHEMA ${nocolor}"
 cp /root/repos-shell/mongodb.repo /etc/yum.repos.d/mongodb.repo
-yum install mongodb-org-shell -y
-mongo --host mongodb-dev.mounika.site </app/schema/user.js
-echo -e "\e[32m ENABLING AND STARTING THE user SERVICE \e[0m"
-systemctl enable user
+yum install mongodb-org-shell -y &>>${logfile}
+mongo --host mongodb-dev.mounika.site <${app_path}/schema/user.js
+echo -e "${color} ENABLING AND STARTING THE user SERVICE ${nocolor}"
+systemctl enable user &>>${logfile}
 systemctl restart user
 
 
