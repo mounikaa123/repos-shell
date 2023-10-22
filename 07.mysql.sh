9add099200ef59fe504ddb91411cf3806890dae0
A <Vijaya1999bsc@gmail.com>
2023-10-22 12:20:42 +0530

    project

--- 07.mysql.sh
+++ 07.mysql.sh
@@ -1,11 +1,16 @@
-echo -e "\e[32m DISABLING MySql SERVICE \e[0m"
-yum module disable mysql -y
-echo -e  "\e[32m SETTING MySql REPO \e[0m"
+color="\e[32m"
+nocolor="\e[0m"
+logfile="/tmp/roboshop.log"
+
+
+echo -e "$color DISABLING MySql SERVICE $nocolor"
+yum module disable mysql -y &>>${logfile}
+echo -e  "$color SETTING MySql REPO $nocolor"
 cp /root/repos-shell/mysql.repo /etc/yum.repos.d/mysql.repo
-echo -e "\e[32m INSTALLING MySql SERVER \e[0m"
-yum install mysql-community-server -y
-echo -e "\e[32m CHANGING THE DEFAULT ROOT PASSWORD \e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1
-echo -e "\e[32m ENABLING AND STARTING THE MYSQL SERVICE \e[0m"
-systemctl enable mysqld
+echo -e "$color INSTALLING MySql SERVER $nocolor"
+yum install mysql-community-server -y &>>${logfile}
+echo -e "$color CHANGING THE DEFAULT ROOT PASSWORD $nocolor"
+mysql_secure_installation --set-root-pass RoboShop@1 &>>${logfile}
+echo -e "$color ENABLING AND STARTING THE MYSQL SERVICE $nocolor"
+systemctl enable mysqld &>>${logfile}
 systemctl restart mysqld
